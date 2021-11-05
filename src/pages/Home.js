c8dfa0a4a1df9738dccb9ae869d945a34f7510af
Jared Fipps <jfipps1995@gmail.com>
2021-11-04 22:34:54 -0700

    initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import CocktailList from '../components/CocktailList'
-import SearchForm from '../components/SearchForm'
+import React from "react";
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 export default function Home() {
   return (
     <main>
+      <h1>Testing</h1>
       <SearchForm />
       <CocktailList />
     </main>
-  )
+  );
 }
